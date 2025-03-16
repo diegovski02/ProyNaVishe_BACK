0109c901187d960c890d8638c6ac03f40b4febb3
Diego Vega <dvegacajas2@gmail.com>
2025-03-16 06:24:42 -0500

    resolviendo problema 1

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void testSuma() {
+        assertEquals(2, 1 + 1);
+    }
+}
